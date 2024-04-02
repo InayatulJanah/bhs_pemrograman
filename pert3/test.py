49f7072864dcb6a7d060a290753dc479f25762ce
InayatulJanah <inahyatuljanah@gmail.com>
2024-04-02 11:32:06 +0700

    test aja

---
+++ pert3/test.py
@@ -0,0 +1,16 @@
+#value_str = input("enter value : ")
+#value = int(value_str)
+value = 50
+
+if value >= 90:
+    grade = "A"
+elif value >= 80:
+    grade = "B"
+elif value >= 70:
+    grade = "C"
+elif value >= 60:
+    grade = "D"
+else :
+    grade = "D"
+
+    print("Grade : %s" %grade)
