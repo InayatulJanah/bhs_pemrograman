025903311db97b6b73a82c6a748852d2ff1fe44a
InayatulJanah <inahyatuljanah@gmail.com>
2024-06-25 09:53:53 +0700

    pesan

--- pert11/product_service/app/api/db.py
+++ pert11/product_service/app/api/db.py
@@ -1,2 +1,2 @@
 from pymongo import MongoClient
-conn = MongoClient('mongodb://root:p455w0rd@192.168.202.171:27017/products?authSource=admin')
+conn = MongoClient('mongodb://root:p455w0rd@172.26.96.1:27017/products?authSource=admin')
